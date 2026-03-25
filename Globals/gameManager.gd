3557fc14ce3b340ba5ab9f7f4ba0a304ea096c82
Mac <tmcpbmay@gmail.com>
2026-03-24 22:26:50 -0400

    We have a landing page which transitions to the battle map

---
+++ Globals/gameManager.gd
@@ -0,0 +1,26 @@
+extends Node
+
+const LANDING = preload("res://Scenes/Landing/landing.tscn")
+const BATTLE_MAP = preload("res://Scenes/Battle Map/battleMap.tscn")
+const CHANGE = preload("res://Scenes/Change/change.tscn")
+
+var nextScene: PackedScene
+var cx: Change
+
+func _ready() -> void:
+	cx = CHANGE.instantiate()
+	add_child(cx)
+
+func change_to_next() -> void:
+	if nextScene:
+		get_tree().change_scene_to_packed(nextScene)
+
+func startTransition(toScene: PackedScene) -> void:
+	nextScene = toScene
+	cx.playAnimation()
+
+func loadLandingScene() -> void:
+	startTransition(LANDING)
+
+func loadBattleScene() -> void:
+	startTransition(BATTLE_MAP)
---
+++ Scenes/Landing/landing.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _on_start_pressed() -> void:
+	GameManager.loadBattleScene()
