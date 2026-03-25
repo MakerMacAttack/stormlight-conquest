97247f1ae257aef4a47951d46edf1fadf5c02464
Mac <tmcpbmay@gmail.com>
2026-03-25 08:48:40 -0400

    Add some constants

---
+++ Globals/Constants.gd
@@ -0,0 +1,54 @@
+extends Node
+
+const DEFAULT_REGIONS = [
+		{
+			"displayName": "Kholinar",
+			"id": 101,
+			"mapX": 0,
+			"mapY": 0,
+			"neighbors": []
+	}
+]
+
+const DEFAULT_KINGDOMS = [
+	{
+		"displayName": "Alethkar",
+		"bonus": 8
+	},
+	{
+		"displayName": "Natanatan",
+		"bonus": 4
+	},
+	{
+		"displayName": "Thaylenah",
+		"bonus": 3
+	},
+	{
+		"displayName": "Herdaz",
+		"bonus": 4
+	},
+	{
+		"displayName": "Jah Keved",
+		"bonus": 6
+	},
+	{
+		"displayName": "Sela Tales",
+		"bonus": 4
+	},
+	{
+		"displayName": "Azish Empire",
+		"bonus": 9
+	},
+	{
+		"displayName": "Shinovar",
+		"bonus": 6
+	},
+	{
+		"displayName": "Aimia",
+		"bonus": 2
+	},
+	{
+		"displayName": "Iri",
+		"bonus": 5
+	},
+]
