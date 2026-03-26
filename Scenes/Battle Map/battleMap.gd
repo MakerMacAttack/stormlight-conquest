252b909269a2422208cf38077cced4cfc225a6ff
Mac <tmcpbmay@gmail.com>
2026-03-25 22:13:52 -0400

    Starting to work on the battlemap pop-up to start the game.

--- Scenes/Battle Map/battleMap.gd
+++ Scenes/Battle Map/battleMap.gd
@@ -4,5 +4,11 @@ extends Control
 @onready var player_select: PopupMenu = $playerSelect
 
 
+var playerColors: Array[PlayerColor]
+
+
 func _ready() -> void:
+	for newColor in Constants.DEFAULT_COLORS:
+		if newColor.displayName != "Neutral":
+			playerColors.append(PlayerColor.new(newColor))
 	player_select.visible = true
